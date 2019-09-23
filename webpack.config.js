5c4e0527daccc4ad20b8d126f24c2d66cc1d4037
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2019-09-23 19:10:32 -0400

    fixed webpack html render

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const htmlPlugin = new HtmlWebpackPlugin({
-// 	template: './src/index.html',
-// 	filename: './index.html'
-// });
-const htmlPlugin = new HtmlWebpackPlugin();
+const htmlPlugin = new HtmlWebpackPlugin({
+	template: './src/index.html',
+	filename: './index.html'
+});
 
 module.exports = {
 	module: {

9f58af95a8d98fa48563b756a0ab7e73ee578680
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2019-09-23 19:01:42 -0400

    added lock file to repo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			url: 'https://github.com/rodrigofuentes/react-webpack'
+		};
+	}
 	render() {
 		return (
 			<div>
-				<p>React-ing</p>
+				<p>React-ing: check out the repo</p>
+				<a href={this.state.url}>github.com/react-webpack</a>
 			</div>
 		);
 	}

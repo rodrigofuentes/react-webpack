aa8a2a10c969ee90fa851041ce31d709c80a7ced
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2019-09-24 03:00:28 -0400

    removed tests

--- src/index.js
+++ src/index.js
@@ -4,6 +4,4 @@ import ReactDOM from 'react-dom';
 import App from './app.js';
 import styles from './main.scss';
 
-import test from './test.js';
-
 ReactDOM.render(<App />, document.getElementById('app'));

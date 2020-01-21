235b7a1c296fd8535372d1085539dd19969fec3e
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2020-01-21 01:05:23 -0500

    converted tabs to spaces

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,29 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const htmlPlugin = new HtmlWebpackPlugin({
-	template: './src/index.html',
-	filename: './index.html'
+  template: './src/index.html',
+  filename: './index.html'
 });
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			},
-			{
-				test: /\.(sa|sc|c)ss$/, // s?[ac]ss => matches .ass files :)
-				use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
-			}
-		]
-	},
-	plugins: [
-		htmlPlugin,
-		new MiniCssExtractPlugin({
-			filename: '[name].css',
-			chunkFilename: '[id].css'
-		})
-	]
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(sa|sc|c)ss$/, // s?[ac]ss => matches .ass files :)
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    htmlPlugin,
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
 };
